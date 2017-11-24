ddb4484c6e8339c8aa8eb970c9e844d76aa19a07
alexandder <aleksanderbolt@yahoo.com>
2017-11-24 13:37:31 +0100

    add cluster graphs

--- code/clusters/cluster_simulation.py
+++ code/clusters/cluster_simulation.py
@@ -2,11 +2,11 @@ import random
 
 import sys
 
+import matplotlib.pyplot as plt
 import lib.ca_lib as ca_lib
 import lib.sca_lib as sca_lib
 import initial_configurations as initial_configurations
 
-#probability of gap introduction vs maximal cluster size
 
 def generate_simulation(N):
     result = []
@@ -78,16 +78,29 @@ def group_by_cluster_size(simulation):
     return result
 
 def perform_simulation_of_maximal_cluster_size():
-    probabilities = [1.0 * x / 100 for x in range(1, 10)]
+    probabilities = [1.0 * x / 100 for x in range(1, 15)]
     N = 49
     result = {}
     for p in probabilities:
         maximal = 0
-        for i in range(100):
+        for i in range(1000):
             maximal_for_simulation = find_maximal_cluster_size(introduce_gaps(generate_simulation(N), p))
             if maximal_for_simulation > maximal:
                 maximal = maximal_for_simulation
         result[p] = maximal
+    return result
+
+def perform_simulation_of_number_of_clusters():
+    probabilities = [1.0 * x / 100 for x in range(1, 15)]
+    N = 49
+    result = {}
+    for p in probabilities:
+        number_for_p = 0
+        for i in range(1000):
+            simulation = introduce_gaps(generate_simulation(N), p)
+            number_for_simulation = find_number_of_all_clusters(simulation)
+            number_for_p += number_for_simulation
+        result[p] = number_for_p / 1000
     return result
 
 
@@ -102,13 +115,37 @@ def calculate_results_for_simulation():
     print(find_number_of_all_clusters(simulation))
     print(group_by_cluster_size(simulation))
 
+
+def make_plot_cluster_size_probability(data, path):
+    plt.plot(list(data.keys()), list(data.values()))
+    plt.xlabel('probability')
+    plt.ylabel('maximal cluster size')
+    plt.title('Maximal cluster size vs probability of introducing gap')
+    plt.savefig(path + 'cluster_size.pdf')
+    plt.close()
+
+def make_plot_for_number_of_clusters(data, path):
+    plt.plot(list(data.keys()), list(data.values()))
+    plt.xlabel('probability')
+    plt.ylabel('number of clusters')
+    plt.title('Number of clusters vs probability of introducing gap')
+    plt.savefig(path + 'cluster_number.pdf')
+    plt.close()
+
 #print(perform_simulation_of_maximal_cluster_size())
-calculate_results_for_simulation()
+#calculate_results_for_simulation()
+
+data = perform_simulation_of_maximal_cluster_size()
+print(data.keys())
+print(data.values())
+make_plot_cluster_size_probability(data, '')
+
+data_cluster_number = perform_simulation_of_number_of_clusters()
+print(data_cluster_number.keys())
+print(data.values())
+make_plot_for_number_of_clusters(data_cluster_number, '')
 
 
 
 
 
-
-
-# probability of gap introduction vs number of clusters
